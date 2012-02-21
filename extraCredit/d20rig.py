ac9ae84284395a88033a3a43aaca78a896f1844e
Matthew Meneghini <pharnavaziani@lavabit.com>
2012-02-21 13:23:44 -0500

    Version 1.5 complete.  Now random-generates abilities, alignment, level, and class; From a seperate csv file (comma-delineated, UTF-8 only) generates BAB, Saves, skills and health based on the random attributes. Still a few bugs, but none reproduced.

--- extraCredit/d20rig.py
+++ extraCredit/d20rig.py
@@ -3,24 +3,12 @@
 import random
 import csv
 
-"""
-# generic example below
-# format = [hit die,fort save,reflex save,will save,base attack bonus,skill points,is_caster]
-# example = [d4,poor,poor,poor,poor,2,FALSE]
-example_class = csv.reader(open("d20ruleset.csv", "rb"))
-
-for row in example_class:
-    print row
-"""
-
-# for class_list[]: BAB=lvl
-
-# if clss_list
-
-print "Welcome to the d20 Random Individual Generator v1.4" # version 1.0 having been my JavaScript version @ the end of Web Design.
+print "Welcome to the d20 Random Individual Generator v1.5" # version 1.0 having been my JavaScript version @ the end of Web Design.
 # version 1.3 released on February 15, 2012
 # version 1.4.5 alpha finished on February 20, 2012
-# release notes:  added CSV support and health point generation for all classes.  Added Saving Throws (experimental) and some attack bonuses.
+# version 1.5 alpha finished on February 21, 2012
+# current version notes: now full support for a CSV sheet which details HD, BAB, Saves, Skill Points, and True/False caster status.
+
 
 # lets_go=raw_input("press any key to generate a random human individual")
 
@@ -55,7 +43,7 @@ print chosen_class,lvl
 """
 Features that need to be added:
 
-> a way to inflate the scores that does not derive from the D&D 3.5 DMG but produces a number closer to traditional Dungeons and Dragons (TM Wizards of the Coast) score generation
+> a native, OGL-friendly score generation that doesn't give underpowered scores.  Score modules would go well, too.
 > a way to make sure that the ability scores do not conflict with class....IE, a Wizard capable of casting level 3 spells should not be generated with Intelligence < 13
 > a module to choose character ability methods.  If this functionality could be added, the user could specify to use certain scores or certain ranges
 
@@ -78,7 +66,8 @@ cha_mod = (charisma - 10) / 2
 
 # prints out the abiilities
 print "Strengh:",strength,"Dexterity:",dexterity,"Constitution:",constitution,"Intelligence:",intelligence,"Wisdom:",wisdom,"Charisma:",charisma
-print str_mod,dex_mod,con_mod,int_mod,wis_mod,cha_mod
+
+# print str_mod,dex_mod,con_mod,int_mod,wis_mod,cha_mod
 
 # score_bonus = (<score> - 10) / 2 # the bonus from an ability score
 
@@ -91,7 +80,7 @@ saves_bad = ['0','0','1','1','1','2','2','2','3','3','3','4','4','4','5','5','5'
 
 # BASE ATTACK BONUS
 BAB_good = lvl
-BAB_average = ['0','1']	# god I hate medium BAB classes.
+BAB_avg = ['0','1','2','3','3','4','5','6','6','7','8','9','9','10','11','12','12','13','14','15','15']	# god I hate medium BAB classes.
 BAB_poor = lvl / 2	# I love that by doing Int math, we auto-round-down just as d20 rules demand.  Yay laziness!
 
 # SKILLS
@@ -116,7 +105,9 @@ bab = 0
 fort = 0
 reflex = 0
 wills = 0
+sp = 2
 hd = 0
+cl = 0
 
 ##############
 # MAIN LOOP
@@ -126,69 +117,48 @@ for row in example_class:
 #    filter(class_parser, example_class) # took me a while to figure out how to manipulate objects from the CSV
     if row[0] in chosen_class:
         hd = ((int(row[1]) / 2) * lvl) + (con_mod * lvl)
-        print "Health:",hd,"hit points"
+
         if row[2] in 'GOOD':
             fort = saves_good[lvl-1]
             fort = int(fort) + con_mod
         else:
             fort = saves_bad[lvl-1]
             fort = int(fort) + con_mod
-        print "Fortitude save =",fort
+
         if row[3] in 'GOOD':
             reflex = saves_good[lvl-1]
             reflex = int(reflex) + dex_mod            
         else:
             reflex = saves_bad[lvl-1]
             reflex = int(reflex) + dex_mod            
-        print "Rexlex Save =",reflex
+
         if row[4] in 'GOOD':
             wills = saves_good[lvl-1]
             wills = int(wills) + wis_mod            
         else:
-            reflex = saves_bad[lvl-1]
-            reflex = int(wills) + wis_mod            
-        print "Will Save =",wills
+            wills = saves_bad[lvl-1]
+            wills = int(wills) + wis_mod            
+
         if row[5] in 'GOOD':
             bab = BAB_good
         elif row[5] in 'POOR':
             bab = BAB_poor
         else:
+            bab = BAB_avg[lvl-1]
+
+        sp = ((int(row[6])) + int_mod) * lvl
+        
+        if row[7] in 'TRUE':
+            cl = lvl
+        else:
             break
 
+
+print "Health:",hd,"hit points"
 print "Base Attack Bonus =",bab
-
-
-
-
-
-
-
-#####################################
-# THIS IS ALL JUNK
-#####################################
-#    else:
-#        print "to be added"
-
-
-
-#    row = str(row
-#    if row.lower() in ['commoner']:
-#        print row
-#if chosen_class is "Commoner":
-#    print example_class.next()
-#    print example_class.next()
-#    for row in ("Commoner" in example_class.next()):
-#        print row
-#        class_row = csvreader.next(example_class)
-#        print row
-#    hd = d4 # next step:  find a way to access a specific row or position
-#else:
-#    print "details to come"
- 
-# hip = hd + (constitution * lvl) # a character's hit point total
-
-# print "Health",hd,"Hit Points"
-
-# for class_list[]: BAB=lvl
-
-# if clss_list
+print "Fortitude save =",fort
+print "Rexlex Save =",reflex
+print "Will Save =",wills
+print sp,"Skill Points"
+if cl > 0:
+    print "Caster Level",cl
